25c9e5d421a294ef7ac6910db21afdc976d71404
Samadhan Arade <84760368+Samadhan9711@users.noreply.github.com>
2021-05-26 12:21:05 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,20 @@
 public class Hello{
+	public void addition(number1,number2)
+	{
+		System.out.println("Addition is : "+(number1+number2));
+	}
+	
 	public static void main(String[] args){
+		Scanner input1 = new Scanner(System.in);
+		System.out.println("Enter First number : ");
+		int num1 = input1.nextInt();
+		
+		Scanner input1 = new Scanner(System.in);
+		System.out.println("Enter Second number : ");
+		int num2 = input2.nextInt();
+		
+		addition(num1,num2);
+		
 		System.out.println("hello World")
 	}
 }
